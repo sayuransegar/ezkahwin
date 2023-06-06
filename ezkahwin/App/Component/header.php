9dfb9525df184f10baff3a804a20925347a19bac
Aimnsbri <96569271+sayuransegar@users.noreply.github.com>
2023-06-06 14:36:20 +0800

    Delete ezkahwin directory

--- ezkahwin/App/Component/header.php
+++
@@ -1,3 +0,0 @@
-<header class="navbar-light bg-light">
-    <img class="w-100 h-auto" src="public/images/banner.jpg" id="imgbanner" />
-</header>
--- ezkahwin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'BusinessServices/controllers/marriageRegistrationController.php';
-
-
-$marriageRegistrationController = new MarriageRegistrationController();
-$marriageRegistrationController->index();
-
-
-?>
